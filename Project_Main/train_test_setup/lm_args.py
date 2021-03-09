dc07e08b586306cff864c2bac80ed615264b48fa
Ben <benf97@gmail.com>
2021-03-09 01:19:02 +0000

    Uploaded lm args

---
+++ Project_Main/train_test_setup/lm_args.py
@@ -0,0 +1,19 @@
+# =============================================================================
+"""Training Hyperparameters"""
+# =============================================================================
+
+xl_args = {"train_batch_size": 12, "learning_rate": 2e-05, "logging_steps": 50,
+           "max_grad_norm": 1.0, "n_gpu": 1, "num_train_epochs": 2, "optimizer": "AdamW",
+           "overwrite_output_dir": True, "adam_epsilon": 1e-08, "polynomial_decay_schedule_lr_end": 1e-07,
+           "polynomial_decay_schedule_power": 1.0, "save_model_every_epoch": True,
+           "save_optimizer_and_scheduler": True, "save_steps": -1, "scheduler": "linear_schedule_with_warmup",
+           "use_multiprocessing": False, "warmup_steps": 8344, "model_class": "ClassificationModel",
+           "labels_list": ["machine", "human"], "labels_map": {"machine": 0, "human": 1}}
+
+db_args = {"train_batch_size": 14, "learning_rate": 2e-05, "logging_steps": 50,
+           "max_grad_norm": 1.0, "n_gpu": 1, "num_train_epochs": 2, "optimizer": "AdamW",
+           "overwrite_output_dir": True, "adam_epsilon": 1e-08, "polynomial_decay_schedule_lr_end": 1e-07,
+           "polynomial_decay_schedule_power": 1.0, "save_model_every_epoch": True,
+           "save_optimizer_and_scheduler": True, "save_steps": -1, "scheduler": "linear_schedule_with_warmup",
+           "use_multiprocessing": False, "warmup_steps": 24000, "model_class": "ClassificationModel",
+           "labels_list": ["machine", "human"], "labels_map": {"machine": 0, "human": 1}}
