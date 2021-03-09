e36a39cfeac29605880c68d362cbb3b6877fe32f
Ben <benf97@gmail.com>
2021-03-09 01:18:51 +0000

    Uploaded CreateTrainTestSplit

---
+++ Project_Main/train_test_setup/CreateTrainTestSplit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Ben Feigenbaum
+# Date Last Updated : 09/03/2021 - 00:38
+# =============================================================================
+"""Creating training and testing dataset for training and evaluation"""
+# =============================================================================
+# Imports
+import pandas as pd
+# =============================================================================
+
+
+def fixnewlines(df):
+    return df['text'].replace(to_replace=[r"\\t|\\n|\\r", "\t|\n|\r"], value=["", ""], regex=True, inplace=True)
+
+
+def createdataset(pkl_name, machine, human):
+    df1 = pd.read_json(machine, lines=True, encoding='utf8').text.to_frame()
+    df2 = pd.read_json(human, lines=True, encoding='utf8').text.to_frame()
+
+    df1, df2 = fixnewlines(df1), fixnewlines(df2)
+    df1['class'], df2['class'] = 'machine', 'human'
+
+    df = pd.concat([df1, df2]).reset_index(drop=True)
+    df.columns = ["text", "labels"]
+    df.to_pickle(pkl_name)
+
+
+_dir = "C:/Users/User/Desktop/Project_Main/data/"
+createdataset("test_df_500000", f'{_dir}{"xl-1542M.test.jsonl"}', f'{_dir}{"webtext.test.jsonl"}')
+createdataset("train_df_500000", f'{_dir}{"xl-1542M.train.jsonl"}', f'{_dir}{"webtext.train.jsonl"}')
