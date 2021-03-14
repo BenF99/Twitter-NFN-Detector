50f586a356ec3ca35a61612e7b445bfac437ed10
Ben <benf97@gmail.com>
2021-03-14 21:46:02 +0000

    variable name and format

--- Project_Main/xlnetdeberta.py
+++ Project_Main/xlnetdeberta.py
@@ -18,7 +18,9 @@ logging.set_verbosity_error()
 class XLNetDeBERTaClassification:
 
     def __init__(self, model, model_dir="D:/Language Models/"):
-        model_pn = {'xlnet': f'{model_dir}{"XLNET-LARGE/"}', 'deberta': f'{model_dir}{"DEBERTA-LARGE/"}'}[model]
+        model_pn = {'xlnet': f'{model_dir}{"XLNET-LARGE/"}',
+                    'deberta': f'{model_dir}{"DEBERTA-LARGE/"}'
+                    }[model]
         self.model = ClassificationModel(model, model_pn, use_cuda=False)
         self.tokenizer = XLNetTokenizer.from_pretrained(model_pn) if model == 'xlnet' \
             else DebertaTokenizer.from_pretrained(model_pn)
@@ -38,5 +40,6 @@ class XLNetDeBERTaClassification:
     def check_probs(self):
         _, logits = self.model.predict([self._text])
         probs = softmax(logits[0])
-        probs_2dp = [float("{:.3f}".format(i)) for i in probs]
-        return probs_2dp, len(self.tokenizer.encode(self._text, add_special_tokens=False))
+        probs_3dp = [float("{:.3f}".format(i)) for i in probs]
+        return probs_3dp, len(self.tokenizer.encode(self._text, add_special_tokens=False))
+
