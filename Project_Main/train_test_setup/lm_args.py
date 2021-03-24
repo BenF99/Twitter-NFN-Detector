b7f2d8c29d64a57dcee7b22530b3d795eb9227c2
Ben <55698282+BenF99@users.noreply.github.com>
2021-03-24 22:18:36 +0000

    Updated best model hp

--- Project_Main/train_test_setup/lm_args.py
+++ Project_Main/train_test_setup/lm_args.py
@@ -2,18 +2,18 @@
 """Training Hyperparameters"""
 # =============================================================================
 
-xl_args = {"train_batch_size": 12, "learning_rate": 2e-05, "logging_steps": 50,
+xl_args = {"train_batch_size": 16, "learning_rate": 1e-05, "logging_steps": 50,
            "max_grad_norm": 1.0, "n_gpu": 1, "num_train_epochs": 2, "optimizer": "AdamW",
            "overwrite_output_dir": True, "adam_epsilon": 1e-08, "polynomial_decay_schedule_lr_end": 1e-07,
            "polynomial_decay_schedule_power": 1.0, "save_model_every_epoch": True,
            "save_optimizer_and_scheduler": True, "save_steps": -1, "scheduler": "linear_schedule_with_warmup",
-           "use_multiprocessing": False, "warmup_steps": 8344, "model_class": "ClassificationModel",
+           "use_multiprocessing": False, "warmup_steps": 100, "model_class": "ClassificationModel",
            "labels_list": ["machine", "human"], "labels_map": {"machine": 0, "human": 1}}
 
-db_args = {"train_batch_size": 16, "learning_rate": 1e-05, "logging_steps": 50,
-           "max_grad_norm": 1.0, "n_gpu": 1, "num_train_epochs": 2, "optimizer": "AdamW",
-           "overwrite_output_dir": True, "adam_epsilon": 1e-08, "polynomial_decay_schedule_lr_end": 1e-07,
-           "polynomial_decay_schedule_power": 1.0, "save_model_every_epoch": True,
+db_args = {"train_batch_size": 16, "learning_rate": 5e-06, "logging_steps": 50,
+           "max_grad_norm": 1.0, "n_gpu": 1, "num_train_epochs": 4, "optimizer": "AdamW",
+           "overwrite_output_dir": True, "adam_epsilon": 1e-06, "polynomial_decay_schedule_lr_end": 1e-07,
+           "polynomial_decay_schedule_power": 1.0, "save_model_every_epoch": True, "weight_decay": 0.01,
            "save_optimizer_and_scheduler": True, "save_steps": -1, "scheduler": "linear_schedule_with_warmup",
-           "use_multiprocessing": False, "warmup_steps": 25000, "model_class": "ClassificationModel",
+           "use_multiprocessing": False, "warmup_steps": 50, "model_class": "ClassificationModel",
            "labels_list": ["machine", "human"], "labels_map": {"machine": 0, "human": 1}}
